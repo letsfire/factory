f1e722ceccad0209a438a9abcb1a52d851bf172a
letsfire <249008728@qq.com>
2019-11-18 23:26:17 +0800

    Fix Issue 0004

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/letsfire/factory
 
 go 1.12
 
-require github.com/stretchr/testify v1.4.0
+require (
+	github.com/letsfire/utils v0.0.0-20191027122615-30a04c61bccf
+	github.com/stretchr/testify v1.4.0
+)
--- master.go
+++ master.go
@@ -3,28 +3,28 @@ package factory
 import (
 	"sync"
 	"sync/atomic"
+
+	"github.com/letsfire/utils"
 )
 
 // Master 管理者角色
 type Master struct {
 	sync.Mutex
-	maxNum  int64
-	cursor  int64
-	idxFlag int32
-	tryFlag int32
-	synCond *sync.Cond
-	workers [2]map[int64]*worker
+	maxNum     int
+	ingNum     int
+	cursor     int64
+	workers    sync.Map
+	resetGuard *utils.Guard
 }
 
 func NewMaster(maxNum, initNum int) *Master {
 	m := new(Master)
-	m.maxNum = int64(maxNum)
-	m.workers[0] = make(map[int64]*worker, initNum)
+	m.maxNum = maxNum
+	m.ingNum = initNum
 	for i := 0; i < initNum; i++ {
-		m.workers[0][int64(i)] = newWorker()
+		m.workers.Store(i, newWorker())
 	}
-	m.workers[1] = m.workers[0]
-	m.synCond = sync.NewCond(&sync.Mutex{})
+	m.resetGuard = utils.NewGuard()
 	return m
 }
 
@@ -33,59 +33,47 @@ func (m *Master) AddLine(name string, action func(interface{})) *Line {
 }
 
 func (m *Master) AdjustSize(newSize int) {
-	if newSize > int(m.maxNum) {
-		newSize = int(m.maxNum)
+	if newSize > m.maxNum {
+		newSize = m.maxNum
 	}
-
 	m.Lock()
 	defer m.Unlock()
-
-	workers := m.usingGroup()
-	diff := newSize - len(workers)
-
-	if diff > 0 {
-		for i := len(workers); i < newSize; i++ {
-			workers[int64(i)] = newWorker()
+	if newSize > m.ingNum {
+		for i := m.ingNum; i < newSize; i++ {
+			m.workers.Store(i, newWorker())
 		}
-	} else if diff < 0 {
-		for i := len(workers); i > newSize; i-- {
-			idx := int64(i) - 1
-			workers[idx].shutdown()
-			delete(workers, idx)
+	} else if newSize < m.ingNum {
+		for i := m.ingNum; i > newSize; i-- {
+			idx := i - 1
+			if v, ok := m.workers.Load(idx); ok {
+				m.workers.Delete(idx)
+				v.(*worker).shutdown()
+			}
 		}
 	}
-
-	idxFlag := 2 + ^m.idxFlag
-	m.workers[idxFlag] = workers
-	atomic.StoreInt32(&m.idxFlag, idxFlag)
-	m.workers[2+^m.idxFlag] = m.workers[idxFlag]
+	m.ingNum = newSize
 }
 
 func (m *Master) Running() int {
-	return len(m.usingGroup())
+	m.Lock()
+	defer m.Unlock()
+	return m.ingNum
 }
 
 func (m *Master) Shutdown() {
 	m.AdjustSize(0) // 关闭所有worker
 }
 
-func (m *Master) usingGroup() map[int64]*worker {
-	return m.workers[atomic.LoadInt32(&m.idxFlag)]
-}
-
 func (m *Master) getWorker() *worker {
-	idx := atomic.AddInt64(&m.cursor, 1)
-	if w, ok := m.usingGroup()[idx-1]; ok && w != nil {
-		return w
+	idx := int(atomic.AddInt64(&m.cursor, 1)) - 1
+	if w, ok := m.workers.Load(idx); ok && w != nil {
+		return w.(*worker)
+	} else if m.ingNum == 0 {
+		panic("factory: the master has been shutdown")
 	}
-	if atomic.CompareAndSwapInt32(&m.tryFlag, 0, 1) {
+	m.resetGuard.Run("get-worker", func() (i interface{}, e error) {
 		atomic.StoreInt64(&m.cursor, 0)
-		atomic.StoreInt32(&m.tryFlag, 0)
-		m.synCond.Broadcast()
-	} else {
-		m.synCond.L.Lock()
-		m.synCond.Wait()
-		m.synCond.L.Unlock()
-	}
+		return nil, nil
+	})
 	return m.getWorker()
 }
